1fba6698441ea0b6edee642b5c817659c5f6cdf7
KRiNGU <alexandr.lobashov@mail.ru>
2021-09-19 00:00:24 +0300

    fix all errors except 18

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -9,22 +9,15 @@ export default class ToDo {
         this.update = update;
     }
 
-    _doubleClickEvent = () => {
-        this.todoText.addEventListener('click', this._toggleInputMod);
-        setTimeout(() => this.todoText.removeEventListener('click', this._toggleInputMod), 400);
-    }
-
     _onChange = () => {
         this.todoText.textContent = this.todoChange.value;
     }
 
-    _onMouseClick = (e) => {
-        if (e.target !== this.todoChange && e.target !== this.todoText) {
-            this._toggleLabelMod();
-            this.update(this.todoText.textContent, this.isCompleted, this.id);
-            if (this.todoChange.value === '') {
-                this._deleteTodo();
-            }
+    _onBlur = (e) => {
+        this._toggleLabelMod();
+        this.update(this.todoText.textContent, this.id);
+        if (!this.todoChange.value) {
+            this._deleteTodo();
         }
     };
 
@@ -35,11 +28,12 @@ export default class ToDo {
 
     _handleComplete = () => {
         this.todo.classList.toggle('todo_complete');
+        this.todo.classList.toggle('todo_incomplete');
         this.todoText.classList.toggle('todo_complete-text');
         this.todoCompleteButton.classList.toggle('todo__button-complete_completed');
         this.todoCompleteButton.textContent = this.todoCompleteButton.textContent === "N" ? "Y" : "N";
         this.isCompleted = !this.isCompleted;
-        this.complete(this.todo, this.text, this.isCompleted, this.id);
+        this.complete(this.todo, this.id);
     }
 
     
@@ -47,8 +41,8 @@ export default class ToDo {
         if (e.code === "Enter") {
             this._toggleLabelMod();
             this.todoText.textContent = this.todoChange.value;
-            this.update(this.todoText.textContent, this.isCompleted, this.id);
-            if (this.todoChange.value === '') { 
+            this.update(this.todoText.textContent, this.id);
+            if (!this.todoChange.value) { 
                 this._deleteTodo();
             }
         }
@@ -61,58 +55,48 @@ export default class ToDo {
     _toggleLabelMod = () => {
         this.todoChange.classList.remove('todo__change-label_active');
         this.todoContainer.classList.add('todo__container_active');
-        this.todoButton.addEventListener('click', this._deleteTodo);
+        this.deleteTodoButton.addEventListener('click', this._deleteTodo);
         this.todoCompleteButton.addEventListener('click', this._handleComplete);
         this.todoChange.removeEventListener('change', this._onChange);
         this.todoChange.removeEventListener('keydown', this._onKeyDown);
-        document.removeEventListener('click', this._onMouseClick);
+        this.todoChange.removeEventListener('blur', this._onBlur);
     }
 
     _toggleInputMod = () => {
         this.todoChange.classList.add('todo__change-label_active');
         this.todoContainer.classList.remove('todo__container_active');
-        this.todoButton.removeEventListener('click', this._deleteTodo);
+        this.deleteTodoButton.removeEventListener('click', this._deleteTodo);
         this.todoCompleteButton.removeEventListener('click', this._handleComplete);
         this.todoChange.addEventListener('change', this._onChange);
         this.todoChange.addEventListener('keydown', this._onKeyDown);
-        document.addEventListener('click', this._onMouseClick);
+        this.todoChange.focus();
+        this.todoChange.addEventListener('blur', this._onBlur);
     }
 
     _setEventListeners() {
-        this.todoButton.addEventListener('click', this._deleteTodo);
+        this.deleteTodoButton.addEventListener('click', this._deleteTodo);
         this.todoCompleteButton.addEventListener('click', this._handleComplete);
-        this.todoText.addEventListener('click', this._doubleClickEvent);
-        this.todo.addEventListener('mouseover', () => {
-            this.todoButton.classList.remove('hidden');
-        })
-        this.todo.addEventListener('mouseout', () => {
-            this.todoButton.classList.add('hidden');
-        })
-
+        this.todoText.addEventListener('dblclick', this._toggleInputMod);
     }
 
     _getTemplate() {
         return document.querySelector(this.tmpSelector).content;
     }
 
-    getText() {
-        return this.text;
-    }
-
-    getId = () => {
-        return this.id;
-    }
+    getId = () => this.id;
 
     getTodo() {
         this.todo = this._getTemplate().querySelector('.todo').cloneNode(true);
         this.todoText = this.todo.querySelector('.todo__text');
         this.todoCompleteButton = this.todo.querySelector('.todo__button-complete');
-        this.todoButton = this.todo.querySelector('.todo__button-delete');
+        this.deleteTodoButton = this.todo.querySelector('.todo__button-delete');
         this.todoChange = this.todo.querySelector('.todo__change-label');
         this.todoContainer = this.todo.querySelector('.todo__container');
         this.todoCompleteButton.textContent = 'N';
+        this.todo.classList.add('todo_incomplete');
         if (this.isCompleted) {
             this.todo.classList.toggle('todo_complete');
+            this.todo.classList.toggle('todo_incomplete');
             this.todoText.classList.toggle('todo_complete-text');
             this.todoCompleteButton.classList.toggle('todo__button-complete_completed');
             this.todoCompleteButton.textContent = this.todoCompleteButton.textContent === "N" ? "Y" : "N";
